7b53ee304c041b8e882e8c066f91b2ffb1563afa
KingLucky13 <60195892+KingLucky13@users.noreply.github.com>
2024-04-04 23:41:51 +0300

    ParticlesAndAudio

--- Assets/Scripts/Shooting/Weapon.cs
+++ Assets/Scripts/Shooting/Weapon.cs
@@ -20,9 +20,18 @@ namespace LearnGame.Shooting
         [SerializeField]
         private float _damage = 1f;
 
+        [SerializeField]
+        private ParticleSystem _shootParticle;
+
+        [SerializeField]
+        private AudioSource _shootAudio;
+
         public void Shoot(Vector3 targetPoint) 
         {
             Bullet bullet = Instantiate(BulletPrefab, _bulletSpawnPosition.position, Quaternion.identity);
+            _shootParticle.Play();
+            _shootAudio.Play();
+
             Vector3 target=targetPoint-_bulletSpawnPosition.position;
             target.y = 0;
             target.Normalize();
